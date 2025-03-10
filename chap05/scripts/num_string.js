9cf4b9d01ea8c9f84bd89fe2e610c1112f5f93aa
fugo8015276 <sf.engineer20231201@gmail.com>
2025-03-10 23:37:04 +0900

    7日目

--- chap05/scripts/num_string.js
+++ chap05/scripts/num_string.js
@@ -1,6 +1,13 @@
 let num1 = 255;
+//16進数に変換
 console.log(num1.toString(16));
+//8進数に変換
 console.log(num1.toString(8));
+//10進数に変換
+console.log(num1.toString());
+//2進数に変換
+console.log(num1.toString(2));
+
 
 let num2 = 123.45678;
 console.log(num2.toExponential(2));
